63e95554cb35c09e9da6ec40ee70262001e88f4d
arman singh chahal <armanchahal530@gmail.com>
2026-09-01 14:53:36 +0530

    Add files via upload

---
+++ backslash.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+	int name;
+	printf("hello world \n");
+	printf("hello\thow\tare\tyou \n");
+	printf("hii\b \n");
+	printf("hogwads\'\n");
+	printf("\"");
+	return 0;
+}
---
+++ ternery operator.cpp
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+	int a ;
+	printf("What is the number \n");
+	scanf("%d",&a);
+	(a%2==0)?printf("The number is even"):printf("The number is odd");
+	return 0;
+}
