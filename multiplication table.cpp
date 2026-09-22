3e73a4f8f31d0cb33dc78ef6b0c7edaa191b4401
arman singh chahal <armanchahal530@gmail.com>
2026-09-22 15:15:55 +0530

    Add files via upload

--- multiplication table.cpp
+++ multiplication table.cpp
@@ -4,9 +4,9 @@ int main()
 	int n,next,i;
 	printf("which table do you want to print");
 	scanf("%d",&n);
-	for(i==1;i<=10;i++){
+	for(i=1;i<=10;i++){
 		next=n*i;
-		printf("%d \n",next);
+		printf("%d*%d=%d \n",n,i,next);
 	}
 	  return 0;
 	  
